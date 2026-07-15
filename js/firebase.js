7e5f360fc68e1cda157fd32ad611f709eaf3acce
Overlookalpha <isaiasfr26@gmail.com>
2026-07-15 21:37:56 +0100

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,21 @@
+// Importa o Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta os serviços
+export const auth = getAuth(app);
+export const db = getFirestore(app);
