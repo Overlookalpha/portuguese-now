292a8514661fcd0db92192920da173b580ce9a02
Overlookalpha <isaiasfr26@gmail.com>
2026-07-17 12:50:10 +0100

    Fix user authentication alert logic

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,14 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+alert("auth.js carregou!");
+
 onAuthStateChanged(auth, (user) => {
 
-    if (!user) {
+    if (user) {
+        alert("Usuário logado: " + user.email);
+    } else {
+        alert("Nenhum usuário logado");
         window.location.href = "login.html";
     }
 
