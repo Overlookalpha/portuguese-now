34a6f60ed08a665cc6fc4d419969b04082d97b94
Overlookalpha <isaiasfr26@gmail.com>
2026-07-17 12:19:22 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyA_pOaq-8gKGn5fLMUkqCFia7vI0LdwMt8",
+  authDomain: "portuguese-now.firebaseapp.com",
+  projectId: "portuguese-now",
+  storageBucket: "portuguese-now.firebasestorage.app",
+  messagingSenderId: "912497033906",
+  appId: "1:912497033906:web:4286b3488510d2f746ffab"
 };
 
 // Inicializa o Firebase
