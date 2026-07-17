127fbda11c51fc4de5f3b316efee5a4b9a6c83ce
Overlookalpha <isaiasfr26@gmail.com>
2026-07-17 12:54:03 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,15 +1,19 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-alert("auth.js carregou!");
-
 onAuthStateChanged(auth, (user) => {
 
-    if (user) {
-        alert("Usuário logado: " + user.email);
-    } else {
-        alert("Nenhum usuário logado");
+    if (!user) {
         window.location.href = "login.html";
+        return;
+    }
+
+    console.log("Usuário logado:", user.email);
+
+    const titulo = document.querySelector(".hero-content h1");
+
+    if (titulo) {
+        titulo.innerHTML = Welcome Back, ${user.displayName || "Student"}! 👋;
     }
 
 });
